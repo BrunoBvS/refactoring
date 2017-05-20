b173efebc856fc1ac00698696fef6af9bb4b57af
Bruno Vieira <brunovieira@zgsolucoes.com.br>
2017-05-20 01:47:06 -0300

    Versionando no meu GIT as refatorações que a Denise versionou até o momento em https://github.com/DeniseCruz/AulaRefatora

--- nf/src/main/java/br/com/bruno_e_denise/nf/GeradorNotaFiscal.java
+++ nf/src/main/java/br/com/bruno_e_denise/nf/GeradorNotaFiscal.java
@@ -1,14 +1,5 @@
 package br.com.bruno_e_denise.nf;
 
-import java.util.Properties;
-
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -21,34 +12,8 @@ public class GeradorNotaFiscal {
 	}
 
 	private void enviarEmail(Fatura f) {
-		final String username = "refatoracaoalfa2017@gmail.com";
-		final String password = "refatoracao123";
-
-		Properties props = new Properties();
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.starttls.enable", "true");
-		props.put("mail.smtp.host", "smtp.gmail.com");
-		props.put("mail.smtp.port", "587");
-
-		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(username, password);
-			}
-		});
-
-		try {
-
-			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress("refatoracaoalfa2017@gmail.com"));
-			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("diegoamericoguedes@gmail.com"));
-			message.setSubject("Titulo");
-			message.setText("Mensagem");
-
-			Transport.send(message);
-
-		} catch (MessagingException e) {
-			throw new RuntimeException(e);
-		}
+		EnviarEmail enviaEmail = new EnviarEmail();
+		enviaEmail.enviarEmail(f);
 	}
 
 	private void armazenarNoBanco(NotaFiscal notaFiscal) {
